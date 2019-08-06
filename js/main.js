eb2d7c04fd49f80c1fcebe634bef29effb0ce466
Debfront <deborah_monteiro12@hotmail.com>
2019-08-05 23:30:49 -0300

    js
    testando o js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+function repeteRepete() {
+    var pandinha = "o nome da pandinha é "
+    var nome = "Deborah"
+    console.log(pandinha + nome)
+
+    pandinha = "é muito linda"
+    console.log(nome + pandinha)
+
+}
+repeteRepete()
+
+function parametro(numero1, numero2) {
+    console.log(numero1+numero2)
+}
+parametro(5,10)
+parametro(12,6)
+
+function caixa(cliente1, cliente2, cliente3) {
+console.log(cliente1+cliente2)
+}
+caixa(5,3)
+caixa(7,1)
+caixa(8,3)
+
+function alertaPreco(textoDoProduto,preco){
+    alert("o seguinte produto " + textoDoProduto + "$"+preco)
+}
+
+
+
