70870bf059f7244969dc58f86eb6f205652a7c6b
Debfront <deborah_monteiro12@hotmail.com>
2019-08-07 09:04:40 -0300

    menu, produtos
    adicionado imagens, menu de navegação e footer

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var MaletaAberta = false;
+function mostrarMaleta() {
+    if (MaletaAberta == false) {
+        $('#maleta').show();
+        return MaletaAberta = true;
+    }
+    if (MaletaAberta == true) {
+        $('#maleta').hide();
+        return MaletaAberta = false;
+    }
+}
