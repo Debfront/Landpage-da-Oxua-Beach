fe9461a0b90f2bb80e5e173f70dddcc3d513148b
Debfront <deborah_monteiro12@hotmail.com>
2019-08-12 01:20:16 -0300

    menu footer
    css

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-$(document).ready(function(){
+var maletaEstaAberta = false;
+
+$(document).ready(function () {
   $('.slide-principal').slick({
-   dots: false,
-   arrows: true,
-   autoplay: true,
-   autoplaySpeed: 3000,
-   nextArrow: '.bolaNext',
-  prevArrow: '.bolaPrev',
-   });
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: '.bolaNext',
+    prevArrow: '.bolaPrev',
+  });
 })
+
+function mostrarMaleta() {
+  if (maletaEstaAberta == false) {
+    $(".menumaleta").show("slow");
+    maletaEstaAberta = true;
+  }
+ else if (maletaEstaAberta == true) {
+    $(".menumaleta").hide("slow");
+    maletaEstaAberta = false;
+  }
+}
+
